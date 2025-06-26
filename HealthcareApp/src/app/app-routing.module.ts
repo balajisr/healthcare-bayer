85b24cecdc133681d7338bcdfe8767b459a9c012
Balaji <iambalaji.sr@gmail.com>
2025-06-26 16:52:17 +0530

    scheduler library

--- HealthcareApp/src/app/app-routing.module.ts
+++ HealthcareApp/src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ViewSchedulerComponent } from './components/view-scheduler/view-scheduler.component';
+import { AttendanceComponent } from './components/attendance/attendance.component';
+
+const routes: Routes = [
+  {
+    path: "viewscheduler", component: ViewSchedulerComponent
+  },
+  {
+    path: "attendance", component: AttendanceComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
--- HealthcareApp/src/app/app.module.ts
+++ HealthcareApp/src/app/app.module.ts
@@ -1,29 +1,42 @@
 import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { CalendarModule, MOMENT, DateAdapter } from 'angular-calendar';
-import { SchedulerModule } from 'angular-calendar-scheduler';
-import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 import moment from 'moment';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { SchedulerModule } from 'angular-calendar-scheduler';
+
+import { ViewSchedulerComponent } from './components/view-scheduler/view-scheduler.component';
+import { AttendanceComponent } from './components/attendance/attendance.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewSchedulerComponent,
+    AttendanceComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
+    // ✅ angular-calendar setup
     CalendarModule.forRoot({
       provide: DateAdapter,
       useFactory: adapterFactory,
     }),
-    SchedulerModule.forRoot({ locale: 'en', headerDateFormat: 'daysRange' })
+
+    // ✅ angular-calendar-scheduler setup
+    SchedulerModule.forRoot({
+      locale: 'en',
+      headerDateFormat: 'daysRange',
+    }),
+
   ],
   providers: [
-        { provide: LOCALE_ID, useValue: 'en-US' },
-        { provide: MOMENT, useValue: moment }
+        { provide: LOCALE_ID, useValue: 'en-US' }
   ],
   bootstrap: [AppComponent]
 })
