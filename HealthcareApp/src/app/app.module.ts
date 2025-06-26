d596453e3238ca43fcdefdb134dde32ff330ba5e
Balaji <iambalaji.sr@gmail.com>
2025-06-26 14:26:28 +0530

    calendar init

--- HealthcareApp/src/app/app.module.ts
+++ HealthcareApp/src/app/app.module.ts
@@ -1,5 +1,8 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CalendarModule, MOMENT } from 'angular-calendar';
+import { SchedulerModule } from 'angular-calendar-scheduler';
+import moment from 'moment';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,9 +13,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CalendarModule.forRoot(),
+    SchedulerModule.forRoot({ locale: 'en', headerDateFormat: 'daysRange' })
   ],
-  providers: [],
+  providers: [
+        { provide: LOCALE_ID, useValue: 'en-US' },
+        { provide: MOMENT, useValue: moment }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
