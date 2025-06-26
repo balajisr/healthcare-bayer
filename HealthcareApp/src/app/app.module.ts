fce224405892d5942bc60e80bb4dd3183f44e2cc
Balaji <iambalaji.sr@gmail.com>
2025-06-26 14:38:31 +0530

    remove ng errors

--- HealthcareApp/src/app/app.module.ts
+++ HealthcareApp/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CalendarModule, MOMENT } from 'angular-calendar';
 import { SchedulerModule } from 'angular-calendar-scheduler';
-import moment from 'moment';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,12 +13,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CalendarModule.forRoot(),
-    SchedulerModule.forRoot({ locale: 'en', headerDateFormat: 'daysRange' })
   ],
   providers: [
-        { provide: LOCALE_ID, useValue: 'en-US' },
-        { provide: MOMENT, useValue: moment }
   ],
   bootstrap: [AppComponent]
 })
